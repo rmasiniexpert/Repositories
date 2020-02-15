bdfb7a0ca642fed58defd986e410fb883e621f64
rmasiniexpert <rmasiniexpert@gmail.com>
2020-02-15 17:39:17 -0500

    Second commit

--- Firstcommit.R
+++ Firstcommit.R
@@ -1,2 +1,8 @@
 q1 <- readline("Hi user! What's your name?")
 print(paste("Hello ", q1))
+q2 <- readline("Are you happy today?")
+if(q2 =="yes"){
+  print("Cool me too")
+}else{
+  print("I'm sorry to know!")
+}
