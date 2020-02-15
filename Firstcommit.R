a170bf1728d1043cf14d0e2084f2f2e290d026fc
Rodrigo Masini <43048583+rmasiniexpert@users.noreply.github.com>
2020-02-15 17:47:50 -0500

    Update Firstcommit.R

--- Firstcommit.R
+++ Firstcommit.R
@@ -5,4 +5,6 @@ if(q2 =="yes"){
   print("Cool me too")
 }else{
   print("I'm sorry to know!")
+q3 <- readline("User, may I ask you why?")
+print("But do you know that you can change the way how are you feel?
 }
